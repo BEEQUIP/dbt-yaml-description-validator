d3e67abd2f2b790a97e78f6b838a67511661ff3d
Ies Timmerarends <ies.timmerarends@beequip.nl>
2025-11-28 16:29:29 +0100

    Fixed set-up

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dbt-yaml-description-validator",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "PyYAML>=5.1",
+        "tqdm"
+    ],
+    entry_points={
+        "console_scripts": [
+            "check-description-ends-with-dot=dbt_yaml_description_validator.check-dot:main"
+        ]
+    },
+)
